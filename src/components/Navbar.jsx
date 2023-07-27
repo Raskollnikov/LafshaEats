8b211a504a31e66f6eccbc876a49109b62633b6c
Raskollnikov <bakhalaia56@gmail.com>
2023-07-27 22:43:41 +0400

    hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
-import React from "react";
-import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import React, { useState } from "react";
+import {
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiOutlineClose,
+  AiFillHeart,
+} from "react-icons/ai";
 import { BsFillCartFill } from "react-icons/bs";
-
+import { TbTruckDelivery } from "react-icons/tb";
+import { LiaWalletSolid } from "react-icons/lia";
+import { BiSolidHelpCircle } from "react-icons/bi";
+import { FaUserFriends, FaGift, FaStar } from "react-icons/fa";
 function Navbar() {
+  const [nav, setNav] = useState(false);
+
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div className="cursor-pointer" onClick={() => setNav(!nav)}>
           <AiOutlineMenu size={30} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
@@ -28,13 +38,66 @@ function Navbar() {
       </div>
 
       {/* cart */}
-      <button className="bg-black text-white hidden md:flex ite ms-center py-2 rounded-full">
+      <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full">
         <BsFillCartFill size={20} className="mr-2" />
         Cart{" "}
       </button>
 
       {/* Mobile */}
-      <div className="bg-black"></div>
+      {nav && (
+        <div className="bg-black/80 fixed w-screen h-screen z-10 top-0  left-0"></div>
+      )}
+
+      {/* Side Menu */}
+      <div
+        className={
+          nav
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300 "
+            : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300 "
+        }
+      >
+        <AiOutlineClose
+          size={30}
+          className="absolute right-4 top-4 cursor-pointer"
+          onClick={() => setNav(!nav)}
+        />
+
+        <h2 className="text-2xl p-4">
+          Lafsha <span className="font-bold">Eats</span>
+        </h2>
+
+        <nav>
+          <ul className="flex flex-col p-4  text-gray-800">
+            <li className="text-xl py-4 flex ">
+              <TbTruckDelivery size={25} className="mr-4" /> Orders
+            </li>
+
+            <li className="text-xl py-4 flex ">
+              <AiFillHeart size={25} className="mr-4" /> Favorites
+            </li>
+
+            <li className="text-xl py-4 flex ">
+              <LiaWalletSolid size={25} className="mr-4" /> Wallet
+            </li>
+
+            <li className="text-xl py-4 flex ">
+              <BiSolidHelpCircle size={25} className="mr-4" /> Help
+            </li>
+
+            <li className="text-xl py-4 flex ">
+              <FaGift size={25} className="mr-4" /> Promotions
+            </li>
+
+            <li className="text-xl py-4 flex ">
+              <FaStar size={25} className="mr-4" /> Best One
+            </li>
+
+            <li className="text-xl py-4 flex ">
+              <FaUserFriends size={25} className="mr-4" /> Invite Friends
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
